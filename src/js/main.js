84849c7f1f9f953525f2ce2c10d654d96a2e1348
Pavel Korol <korol.paul@gmail.com>
2022-11-06 16:56:48 +0300

    pages

--- src/js/main.js
+++ src/js/main.js
@@ -4,27 +4,19 @@ const isMobile = window.innerWidth <= 768
 const isDesktop = window.innerWidth >= 1000
 
 // sliders
-const teamSlider = document.querySelectorAll('.team_slider');
-teamSlider.forEach(el => {
+const roadSlider = document.querySelectorAll('.road-carousel_slides');
+roadSlider.forEach(el => {
     tns({
         container: el,
         items: 1,
+        center: true,
         gutter: 16,
         mouseDrag: true,
         autoplay: false,
         nav: true,
         navPosition: 'bottom',
-        controls: true,
+        controls: false,
         loop: false,
-        disable: true,
-        responsive: {
-            1000: {
-                disable: false,
-                items: 4,
-                gutter: 0,
-                autoWidth: true,
-            }
-        }
     });
 })
 
@@ -52,32 +44,6 @@ function closeAllOpened() {
 const menuLinkElements = document.querySelectorAll('.menu_link, .menu_close');
 menuLinkElements.forEach(el => el.addEventListener('click', () => document.body.classList.remove('menu-opened')));
 
-const menuAnchorsElements = document.querySelectorAll('a[name]');
-menuAnchorsElements.forEach(el => {
-    function setActiveMenuLink(name) {
-        menuLinkElements.forEach(el => el.classList.remove('active'));
-        let link = document.querySelector(`.menu_link[href="#${name}"]`);
-        if(link) {
-            link.classList.add('active');
-        }
-    }
-
-    const observerCallback = function (e) {
-        const { target, intersectionRatio } = e[0];
-        const name = target.getAttribute('name');
-
-        if (intersectionRatio > 0.5) {
-            setActiveMenuLink(name);
-        }
-    };
-
-    const observer = new IntersectionObserver(observerCallback, {
-        rootMargin: '0px 0px 0px 0px',
-        threshold: thresholdSteps,
-        root: null
-    });
-    observer.observe(el);
-});
 
 /* Popup */
 const popupToggleElements = document.querySelectorAll('.js-popup-toggle');
@@ -158,40 +124,6 @@ function initTabs() {
 
 initTabs()
 
-// Persons hover
-const personSliderElements = document.querySelectorAll('.team_slider-image-wrapper');
-const personElements = document.querySelectorAll('.person');
-
-function highlightPerson(e) {
-    const { currentTarget } = e;
-    personSliderElements.forEach(p => p.classList.remove('active'));
-    currentTarget.classList.add('active')
-
-    const slide = currentTarget.dataset.slide;
-    personElements.forEach(p => p.classList.remove('active'));
-    document.querySelector(`.person[data-slide="${slide}"]`).classList.add('active');
-}
-
-personSliderElements.forEach(el => {
-    el.addEventListener('mouseenter', highlightPerson);
-    el.addEventListener('click', highlightPerson);
-});
-
-const personContentElements = document.querySelectorAll('.person_content');
-let personContentHeight = 0;
-
-if (isDesktop) {
-    personContentElements.forEach(el => {
-        const { height } = el.getBoundingClientRect();
-        if (height > personContentHeight) {
-            personContentHeight = height;
-        }
-    })
-    if (personContentHeight) {
-        document.querySelector('.team_slider').style.paddingBottom = personContentHeight + 80;
-    }
-}
-
 
 
 /* cookies */
@@ -215,111 +147,9 @@ if (isDesktop) {
 //     }
 // }
 
-/* typing */
-const typingElements = document.querySelectorAll('.typing');
-function type(element) {
-    const text = element.textContent;
-    element.innerText = '';
-
-    text.split('').forEach((char, index) => {
-        setTimeout(() => {
-            element.textContent = element.textContent + char;
-        }, 70 * index);
-    })
-
-}
-setTimeout(() => {
-    typingElements.forEach(el => {
-        type(el);
-    });
-}, 400)
-
-/* vacancies table */
-function vacancyEl(){
-    const vacancyElements = document.querySelectorAll('.vacancy');
-    const vacanciesIndicatorElement = document.querySelector('.vacancies_indicator');
-
-    function activateRow(e) {
-        const { height } = e.currentTarget.getBoundingClientRect();
-        const { offsetTop } = e.currentTarget;
-        const indicatorHeight = vacanciesIndicatorElement.getBoundingClientRect().height
-
-        vacanciesIndicatorElement.style.clipPath = `inset(${offsetTop}px 0 ${indicatorHeight - offsetTop - height - 2}px 0)`;
-
-        vacancyElements.forEach(el => el.classList.remove('active'));
-        e.currentTarget.classList.add('active');
-    }
-
-    vacancyElements.forEach(el => {
-        el.addEventListener('mouseenter', activateRow);
-    });
-}
-vacancyEl();
-
-
-/* carousel slider */
-const carouselSliders = document.querySelectorAll('.carousel');
-const carouselRows = document.querySelectorAll('.carousel_row');
-
-carouselSliders.forEach(carousel => {
-    function scrollRow(e) {
-        const isDown = e.deltaY > 0;
-        const step = 40;
-
-        function translate(el, isReverse) {
-            const isForward = isReverse ? !isDown : isDown;
-            const transformString = el.style.transform;
-            const previuosX = transformString.substring(transformString.indexOf('(') + 1, transformString.indexOf('px'));
-
-            if (isForward) {
-                el.style.transform = `translateX(${Number(previuosX) - step}px)`;
-            } else {
-                el.style.transform = `translateX(${Number(previuosX) + step}px)`;
-            }
-        }
-
-        if (carouselRows[0]) {
-            translate(carouselRows[0])
-        }
-        if (carouselRows[1]) {
-            translate(carouselRows[1], true)
-        }
-        if (carouselRows[2]) {
-            translate(carouselRows[2])
-        }
-        if (carouselRows[3]) {
-            translate(carouselRows[3], true)
-        }
-    }
-
-    const observerCallback = function (e) {
-        const { boundingClientRect, intersectionRatio } = e[0];
-        // const ratio = boundingClientRect.height / 2 - boundingClientRect.y;
-
-        if (intersectionRatio > 0.1) {
-            document.addEventListener(wheelEvent, scrollRow)
-        } else {
-            document.removeEventListener(wheelEvent, scrollRow)
-        }
-    };
-
-    const observer = new IntersectionObserver(observerCallback, {
-        rootMargin: '0px 0px 0px 0px',
-        threshold: thresholdSteps,
-        root: null
-    });
-    observer.observe(carousel);
-});
-
-carouselRows.forEach((el) => {
-    el.style.transform = 'translateX(0)'
-    lightGallery(el, {
-        speed: 500,
-    });
-});
 
 /* animation */
-const animatedElements = document.querySelectorAll('.vacancies, .task, .person_header, .carousel');
+const animatedElements = document.querySelectorAll('.none');
 animateElements();
 function animateElements() {
     animatedElements.forEach(el => {
@@ -339,4 +169,16 @@ function animateElements() {
     });
 }
 
-carouselRows.forEach((el) => el.style.transform = 'translateX(0)');
+/* faq accordion */
+const faqElements = document.querySelectorAll('.faq_header');
+faqElements.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+        faqElements.forEach(el => {
+            if (el !== e.target) {
+                el.parentElement.classList.remove('opened')
+            }
+        });
+        e.currentTarget.parentElement.classList.toggle('opened');
+    });
+});
